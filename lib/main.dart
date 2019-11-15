bbce7023f6009715532035f24dfafa41156fe50b
oussamabonnor1 <oussamabonnor@gmail.com>
2019-11-15 22:05:01 +0100

    adding navigation between scenes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_design_showcase/profile_scene.dart';
 import 'package:flutter_design_showcase/QuotesScene/quotes_scene.dart';
+import 'package:flutter_design_showcase/profile_scene.dart';
 import 'package:flutter_design_showcase/tourism_scene.dart';
 
 void main() {
@@ -15,7 +15,27 @@ class MainScene extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: QuotesScene(),
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        menuButton(context, QuotesScene(), "Quotes Showcase"),
+        menuButton(context, ProfileScene(), "Profile Showcase"),
+        menuButton(context, TourismScene(), "Tourism Showcase"),
+      ],
+    ));
+  }
+
+  Widget menuButton(BuildContext context, Widget scene, String text) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => scene),
+          );
+        },
+        child: Text(text),
+      ),
     );
   }
 }
