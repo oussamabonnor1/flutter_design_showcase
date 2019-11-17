076ec37409c6bb56f6cd3e1e5352990786150a84
oussamabonnor1 <oussamabonnor@gmail.com>
2019-11-17 01:23:00 +0100

    adding images to cards backgrounds

--- lib/CoursesScene/CourseCard.dart
+++ lib/CoursesScene/CourseCard.dart
@@ -10,42 +10,52 @@ class CourseCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 300,
+      width: 275,
       child: Card(
+        semanticContainer: true,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(50),
         ),
-        color: course.color,
         margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Center(
-                child: Text(
-                  course.course,
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Image(
+              image: AssetImage("images/dude1.jpg"),
+              fit: BoxFit.cover,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Center(
+                  child: Text(
+                    course.course,
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    softWrap: true,
                   ),
-                  softWrap: true,
                 ),
-              ),
-              Center(
-                child: Text(
-                  "By " + course.teacher,
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.grey[400],
-                    fontStyle: FontStyle.italic,
+                Center(
+                  child: Text(
+                    "By " + course.teacher,
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
                 ),
-              ),
-            ],
-          ),
+                SizedBox(
+                  height: 30,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
