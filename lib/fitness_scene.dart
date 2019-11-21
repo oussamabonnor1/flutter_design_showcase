1ff56af00c5ab1aedae185be0b858539c41064a6
oussamabonnor1 <oussamabonnor@gmail.com>
2019-11-21 17:55:32 +0100

    header for social profile done

--- lib/fitness_scene.dart
+++ lib/fitness_scene.dart
@@ -6,13 +6,69 @@ class FitnessScene extends StatefulWidget {
 }
 
 class _FitnessSceneState extends State<FitnessScene> {
-
   Widget header = Row(
-    mainAxisAlignment: MainAxisAlignment.center,
     children: <Widget>[
       CircleAvatar(
         backgroundImage: AssetImage("images/instructor.png"),
         radius: 60,
+      ),
+      Expanded(
+        child: Column(
+          children: <Widget>[
+            Text(
+              "Carley Jones",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "1280",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(
+                      "Followers",
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "477",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(
+                      "Following",
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       )
     ],
   );
@@ -21,20 +77,25 @@ class _FitnessSceneState extends State<FitnessScene> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Fitness Showcase", style: TextStyle(color: Colors.black87),),
+        title: Text(
+          "Fitness Showcase",
+          style: TextStyle(color: Colors.black87),
+        ),
         centerTitle: true,
         elevation: 0,
         backgroundColor: Color(0xFFE8E8E8),
       ),
-      body:
-        Container(
-          color: Color(0xFFE8E8E8),
-          child: Column(
-              children: <Widget>[
-                header,
-              ],
-          ),
+      body: Container(
+        color: Color(0xFFE8E8E8),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: header,
+            ),
+          ],
         ),
+      ),
     );
   }
 }
