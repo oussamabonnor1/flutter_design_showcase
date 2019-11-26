a18527f0eed511f3969fe6ebfc641f338a9fa32a
oussamabonnor1 <oussamabonnor@gmail.com>
2019-11-26 15:31:40 +0100

    adding category cards

--- lib/ShoppingScene/shopping_scene.dart
+++ lib/ShoppingScene/shopping_scene.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 class ShoppingScene extends StatefulWidget {
   static Color backgroundColor = Color(0xFF7C9A9A);
   static Color highlightColor = Color(0xFF8CA7A2);
+  static Color unselectedColor = Color(0xFF68837E);
 
   @override
   _ShoppingSceneState createState() => _ShoppingSceneState();
 }
 
 class _ShoppingSceneState extends State<ShoppingScene> {
+  List<String> categories = ["All", "Shoes", "Cars", "Food", "Pets", "Drinks"];
+  int selectedCategories = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,20 +26,18 @@ class _ShoppingSceneState extends State<ShoppingScene> {
       ),
       body: Container(
         color: ShoppingScene.backgroundColor,
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-          child: Column(
-            children: <Widget>[
-              TextField(
-                style: TextStyle(color: Colors.white, fontSize: 22),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(24, 16, 24, 0),
+              child: TextField(
+                style: TextStyle(color: Colors.white, fontSize: 20),
                 cursorColor: Colors.white,
                 decoration: InputDecoration(
                   fillColor: ShoppingScene.highlightColor,
                   filled: true,
                   border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: ShoppingScene.highlightColor,
-                    ),
+                    borderSide: BorderSide(color: ShoppingScene.highlightColor),
                     borderRadius: BorderRadius.circular(18),
                   ),
                   focusedBorder: OutlineInputBorder(
@@ -47,11 +49,104 @@ class _ShoppingSceneState extends State<ShoppingScene> {
                     borderRadius: BorderRadius.circular(18),
                   ),
                   hintText: "Search",
-                  hintStyle: TextStyle(color: Colors.white, fontSize: 22),
+                  hintStyle: TextStyle(color: Colors.white, fontSize: 20),
                   prefixIcon: Icon(Icons.search, color: Colors.white, size: 28),
                 ),
               ),
-            ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(24, 8, 24, 0),
+              child: Container(
+                height: 50,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: categories.length,
+                    itemBuilder: (context, index) {
+                      return index == selectedCategories
+                          ? selectedCategoryCard(categories[index])
+                          : unselectedCategoryCard(categories[index], index);
+                    }),
+              ),
+            ),
+            Expanded(
+                child: Stack(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: SizedBox(),
+                    ),
+                    Expanded(
+                      flex: 4,
+                      child: Card(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(24),
+                          topRight: Radius.circular(24),
+                        )),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget selectedCategoryCard(String category) {
+    return GestureDetector(
+      onTap: () {},
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        color: ShoppingScene.highlightColor,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
+          child: Center(
+            child: Text(
+              category,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget unselectedCategoryCard(String category, int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedCategories = index;
+        });
+      },
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        color: Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+          child: Center(
+            child: Text(
+              category,
+              style: TextStyle(
+                  color: ShoppingScene.unselectedColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18),
+            ),
           ),
         ),
       ),
